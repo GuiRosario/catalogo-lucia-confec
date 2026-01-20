25ffa60daba12a4ffbe3e1f012177abf4510734b
GuiRosario <gdasilvarosario@gmail.com>
2026-01-20 19:31:53 -0300

    fix key problem

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: 'gn3r7y9o',
+    projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
     dataset: 'production',
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: '2023-05-03', // use current date (YYYY-MM-DD) to target the latest API version
-    token: 'skDiL94MoAzZk6iUHOoK44SBFhE3cxGTakFyWgNvpoxMVZstyVf7aJS9dlsttqv9NRh1GtpaSSOFHg1b6tx2fSQcjQ40ilMAATbYmNB9FIAFrfKcCtr8qvNf3ibgW5ZeOHLylFYBblTlDz3Dh0HPCwtYncK7aRlgUpyLdHUXzyRuvjXlSp5S', // Cached for write operations
+    token: import.meta.env.VITE_SANITY_TOKEN, // Protected via .env
 });
 
 const builder = imageUrlBuilder(client);
